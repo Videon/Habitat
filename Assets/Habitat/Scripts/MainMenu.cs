12e0342022073fdebe9dae858a135d0d71cdeb63
c0mby <comby.ayala@gmail.com>
2021-10-26 00:39:31 +0200

    Added assets for main menu

---
+++ Assets/Habitat/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("StartGame");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+
+    }
+
+   
+}
